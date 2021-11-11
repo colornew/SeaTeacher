45469489b09c1dde3accfa9a67729db6e22575f8
Danil <71227522+51Sirius@users.noreply.github.com>
2021-11-11 23:55:37 +0300

    Create main pages and import frameworks for flask

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort, flash
+from flask_wtf import form
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+import datetime
+import locale
+from os import environ
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', title='MainPage')
 
 
 if __name__ == '__main__':
