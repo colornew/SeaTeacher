afeb3d7939e92c104ab83d36c612aa29d85e9368
51Sirius <71227522+51Sirius@users.noreply.github.com>
2021-12-18 19:20:03 +0300

    Add placeholders for auth

--- functions/forms.py
+++ functions/forms.py
@@ -5,18 +5,18 @@ from wtforms.widgets import TextArea, TextInput, PasswordInput, ListWidget
 
 
 class Registration(FlaskForm):
-    username = StringField('Никнэйм', validators=[DataRequired()], widget=TextInput())
-    firstName = StringField('Имя', validators=[DataRequired()], widget=TextInput())
-    secondName = StringField('Фамилия', validators=[DataRequired()], widget=TextInput())
-    email = StringField('Почта', validators=[DataRequired(), Email()], widget=TextInput())
-    password = StringField('Пароль', validators=[DataRequired()], widget=PasswordInput())
+    username = StringField('Никнэйм', validators=[DataRequired()], widget=TextInput(), render_kw={"placeholder": "Никнейм"})
+    firstName = StringField('Имя', validators=[DataRequired()], widget=TextInput(), render_kw={"placeholder": "Имя"})
+    secondName = StringField('Фамилия', validators=[DataRequired()], widget=TextInput(), render_kw={"placeholder": "Фамилия"})
+    email = StringField('Почта', validators=[DataRequired(), Email()], widget=TextInput(), render_kw={"placeholder": "Почта"})
+    password = StringField('Пароль', validators=[DataRequired()], widget=PasswordInput(), render_kw={"placeholder": "Пароль"})
     password_confirm = StringField('Подтверждение пароля', validators=[DataRequired(), EqualTo('password')],
-                                   widget=PasswordInput())
+                                   widget=PasswordInput(), render_kw={"placeholder": "Повторите пароль"})
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Никнэйм', validators=[DataRequired()], widget=TextInput())
-    password = PasswordField('Пароль', validators=[DataRequired()], widget=PasswordInput())
+    username = StringField('Никнэйм', validators=[DataRequired()], widget=TextInput(), render_kw={"placeholder": "Никнейм"})
+    password = PasswordField('Пароль', validators=[DataRequired()], widget=PasswordInput(), render_kw={"placeholder": "Пароль"})
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
