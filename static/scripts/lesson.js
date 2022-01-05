504eb5d0c95bb38b6f8f2102ff70d16d3882f47d
51Sirius <71227522+51sirius@users.noreply.github.com>
2022-01-05 12:42:41 +0300

    Function for create block another format

--- static/scripts/lesson.js
+++ static/scripts/lesson.js
@@ -7,6 +7,7 @@ let text = blockContent.textContent;
 blockContent.textContent = ''
 let style_check = false
 let cont = document.createElement('div')
+cont.className = 'content-block'
 let style = ''
 for (let i = 0; i < text.length + 1; i++) {
     let char = text.charAt(i)
@@ -16,9 +17,7 @@ for (let i = 0; i < text.length + 1; i++) {
             cont = document.getElementsByClassName('content-block')[count_block]
             cont.innerHTML = txt
             count_block++
-            cont = document.createElement('div')
             txt = ''
-            cont.className = 'content-block'
             style_check = true
             first = false
             style = '"'
@@ -27,14 +26,35 @@ for (let i = 0; i < text.length + 1; i++) {
             style_check = false
             first = true
             st = JSON.parse(style)
-            for (let attribute in st) {
-                alert(attribute)
-                if (attribute === 'format') {
-                    alert(st[attribute])
-                    if (st[attribute] === 'txt') {
-                        alert(st[attribute])
-                        cont.className += ' txt'
+            let attr = st.format
+            if (attr === 'format') {
+                    if (st[attr] === 'txt') {cont.className += ' txt'
+                        cont = document.createElement('div')
                     }
+                    else if (st[attr] === 'photo') {cont.className += ' photo'
+                        cont = document.createElement('img')
+                    }
+                    else if (st[attr] === 'audio') {cont.className += ' audio'
+                        cont = document.createElement('audio')
+                    }
+                    else if (st[attr] === 'video') {cont.className += ' video'
+                        cont = document.createElement('video')
+                    }
+                    else {
+                        cont = document.createElement('div')
+                    }
+                    cont.className = 'content-block'
+                }
+            for (let attribute in st) {
+                if (attribute === 'size'){
+                    if (st[attribute] === '1') {cont.className += ' small'}
+                    else if (st[attribute] === '2') {cont.className += ' medium'}
+                    else if (st[attribute] === '3') {cont.className += ' large'}
+                }
+                if (attribute === 'color'){
+                    if (st[attribute] === 'blue') {cont.className += ' blue'}
+                    else if (st[attribute] === 'red') {cont.className += ' red'}
+                    else if (st[attribute] === 'green') {cont.className += ' green'}
                 }
             }
 
