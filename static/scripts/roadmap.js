a03bed91d8d1789dc26c25321ac0360dfafc5275
51Sirius <71227522+51sirius@users.noreply.github.com>
2022-01-26 00:31:16 +0300

    Add description to islands

--- static/scripts/roadmap.js
+++ static/scripts/roadmap.js
@@ -1,5 +1,5 @@
 function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 }
 
 
@@ -10,3 +10,20 @@ for (let i = 0; i < islands.length; i++) {
     island.style.marginTop = height * getRandomInt(4) + 'px'
 }
 
+let descriptions = document.getElementsByTagName('main')[0].getElementsByClassName('descr')
+
+for (let i = 0; i < islands.length; i++) {
+    islands[i].addEventListener("mouseenter", {handleEvent: show_tip, ind: i});
+    islands[i].addEventListener("mouseleave", {handleEvent: hide_tip, ind: i});
+}
+
+function show_tip() {
+    let index = this.ind
+    descriptions[index].style.display = "block";
+}
+
+
+function hide_tip() {
+    let index = this.ind
+    descriptions[index].style.display = "none";
+}
